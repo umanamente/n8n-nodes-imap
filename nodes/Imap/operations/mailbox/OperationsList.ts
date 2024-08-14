62abb2377e345ce1c13a9eadedbe6454025894b1
jreyesr <jreyesr@users.noreply.github.com>
2024-08-13 22:19:04 -0500

    feat(mailbox): add "Get Quota" operation
    (cherry picked from commit e5152ae8ba0047972d5bf68cb48ee6eccad88c48)

--- nodes/Imap/operations/mailbox/OperationsList.ts
+++ nodes/Imap/operations/mailbox/OperationsList.ts
@@ -2,6 +2,7 @@ import { IResourceDef } from "../../utils/CommonDefinitions";
 import { resourceMailbox } from "./ResourceName";
 import { createMailboxOperation } from "./functions/MailboxCreate";
 import { getMailboxListOperation } from "./functions/MailboxGetList";
+import {getMailboxQuotaOperation} from "./functions/MailboxGetQuota";
 import { getMailboxStatusOperation } from "./functions/MailboxGetStatus";
 import { renameMailboxOperation } from "./functions/MailboxRename";
 
@@ -9,6 +10,7 @@ export const mailboxResourceDefinitions: IResourceDef = {
   resource: resourceMailbox,
   operationDefs: [
     getMailboxListOperation,
+		getMailboxQuotaOperation,
     getMailboxStatusOperation,
     createMailboxOperation,
     renameMailboxOperation,
---
+++ nodes/Imap/operations/mailbox/functions/MailboxGetQuota.ts
@@ -0,0 +1,34 @@
+import { IExecuteFunctions, INodeExecutionData } from 'n8n-workflow';
+import {ImapFlow} from 'imapflow';
+import { IResourceOperationDef } from '../../../utils/CommonDefinitions';
+import { getMailboxPathFromNodeParameter, parameterSelectMailbox } from '../../../utils/SearchFieldParameters';
+
+export const getMailboxQuotaOperation: IResourceOperationDef = {
+  operation: {
+    name: 'Get Quota',
+    value: 'getMailboxQuota',
+    description: 'Get quota (space usage) of an account',
+  },
+  parameters: [
+    {
+      ...parameterSelectMailbox,
+      description: 'Select the mailbox',
+			hint: "Leave as INBOX unless your email provider supports per-folder quotas"
+    },
+  ],
+  async executeImapAction(context: IExecuteFunctions, itemIndex: number, client: ImapFlow): Promise<INodeExecutionData[] | null> {
+		let returnData: INodeExecutionData[] = [];
+		const mailboxPath = getMailboxPathFromNodeParameter(context, itemIndex);
+
+    let info = await client.getQuota(mailboxPath);
+		if (info === false || info === undefined) {
+			// do nothing, returnData is left as an empty array (0 items)
+		} else {
+			let item_json = JSON.parse(JSON.stringify(info));
+			returnData.push({
+				json: item_json,
+			});
+		}
+    return returnData;
+  },
+};
