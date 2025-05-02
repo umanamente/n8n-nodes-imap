1e8ee75721a527913c4a615c22889fd38e9c0dd3
waynemorphic <wayneklyde@gmail.com>
2025-05-02 15:17:59 +0300

    refactor: Refactored changes to the reply feature

--- nodes/Imap/operations/email/functions/EmailReply.ts
+++ nodes/Imap/operations/email/functions/EmailReply.ts
@@ -1,7 +1,15 @@
-import { AppendResonseObject, ImapFlow } from "imapflow";
-import { IExecuteFunctions, INodeExecutionData, NodeApiError, NodeExecutionWithMetadata } from "n8n-workflow";
-import { IResourceOperationDef } from "../../../utils/CommonDefinitions";
-import { getMailboxPathFromNodeParameter, parameterSelectMailbox } from "../../../utils/SearchFieldParameters";
+import { AppendResonseObject, ImapFlow } from 'imapflow';
+import {
+	IExecuteFunctions,
+	INodeExecutionData,
+	NodeApiError,
+	NodeExecutionWithMetadata,
+} from 'n8n-workflow';
+import { IResourceOperationDef } from '../../../utils/CommonDefinitions';
+import {
+	getMailboxPathFromNodeParameter,
+	parameterSelectMailbox,
+} from '../../../utils/SearchFieldParameters';
 import * as nodemailer from 'nodemailer';
 
 const PARAM_NAME_DESTINATION_MAILBOX = 'destinationMailbox';
@@ -9,7 +17,7 @@ const PARAM_NAME_DESTINATION_MAILBOX = 'destinationMailbox';
 export const createReplyOperation: IResourceOperationDef = {
 	operation: {
 		name: 'Reply',
-		value: 'Reply'
+		value: 'Reply',
 	},
 	parameters: [
 		{
@@ -18,79 +26,89 @@ export const createReplyOperation: IResourceOperationDef = {
 			name: PARAM_NAME_DESTINATION_MAILBOX,
 		},
 		{
-			displayName: 'Message',
-			name: 'messageId',
-			type: 'resourceLocator',
-			default: { mode: 'list', value: '' },
-			required: true,
-			modes: [
-				{
-					displayName: 'From List',
-					name: 'list',
-					type: 'list',
-					placeholder: 'Select a message...',
-					typeOptions: {
-						searchListMethod: 'searchMessages',
-						searchable: true,
-					},
-				},
-				{
-					displayName: 'ID',
-					name: 'id',
-					type: 'string',
-					placeholder: 'e.g. AAAkAAAhAAA0BBc5LLLwOOOtNNNkZS05Nz...',
-				},
-			],
+			displayName: 'Email UID',
+			name: 'uid',
+			type: 'string',
+			default: '*',
+			placeholder: 'e.g. 1234',
+			description: 'Email UID to create a reply for',
 		},
 		{
-      displayName: 'From',
-      name: 'from',
-      type: 'string',
-      default: '',
-      description: 'The email address of the sender',
-    },
-		{
-			displayName: 'To',
-			name: 'toRecipients',
-			description: 'Comma-separated list of email addresses of recipients',
+			displayName: 'From Email',
+			name: 'from',
 			type: 'string',
 			default: '',
+			description: 'The email address of the sender',
 		},
 		{
-      displayName: 'Text',
-      name: 'text',
-      type: 'string',
-      default: '',
-      description: 'Email message body content',
-      typeOptions: {
-        rows: 5,
-      },
-    },
+			displayName: 'Text',
+			name: 'text',
+			type: 'string',
+			default: '',
+			description: 'Email message body content',
+			typeOptions: {
+				rows: 5,
+			},
+		},
 	],
-	async executeImapAction(context: IExecuteFunctions, itemIndex: number, client: ImapFlow): Promise<INodeExecutionData[] | NodeExecutionWithMetadata[] | null> {
-
+	async executeImapAction(
+		context: IExecuteFunctions,
+		itemIndex: number,
+		client: ImapFlow,
+	): Promise<INodeExecutionData[] | NodeExecutionWithMetadata[] | null> {
 		let returnData: INodeExecutionData[] = [];
 
-		const destinationMailboxPath = getMailboxPathFromNodeParameter(context, itemIndex, PARAM_NAME_DESTINATION_MAILBOX);
+		const destinationMailboxPath = getMailboxPathFromNodeParameter(
+			context,
+			itemIndex,
+			PARAM_NAME_DESTINATION_MAILBOX,
+		);
 
 		let transporter = nodemailer.createTransport({
-				streamTransport: true,
-				buffer: true,
-				newline: 'unix'
-		})
+			streamTransport: true,
+			buffer: true,
+			newline: 'unix',
+		});
 
-		const messageId = context.getNodeParameter('messageId', itemIndex, undefined, {
+		const uid = context.getNodeParameter('uid', itemIndex, undefined, {
 			extractValue: true,
 		}) as string;
 		const from = context.getNodeParameter('from', itemIndex) as string;
-		const toRecipients = context.getNodeParameter('to', itemIndex) as string;
 		const text = context.getNodeParameter('text', itemIndex) as string;
 
+		await client.mailboxOpen(destinationMailboxPath, { readOnly: false });
+
+		const originalMessage = await client.fetchOne(uid, { envelope: true }, { uid: true });
+		const envelope = originalMessage?.envelope;
+		console.log('Original message', originalMessage);
+		console.log('Envelope', envelope);
+
+		if (!originalMessage) {
+			throw new NodeApiError(
+				context.getNode(),
+				{},
+				{
+					message: 'Original message to reply to not found or envelope is missing.',
+				},
+			);
+		}
+
+		const toAddress =
+			envelope.from && envelope.from.length > 0
+				? `${envelope.from[0].name} <${envelope.from[0].address}>`
+				: '';
+
+		const senderAddress = from.length > 0
+			? `${envelope.to[0].name} <${from}>`
+			: '';
+		console.log('To address', toAddress, "From address", senderAddress);
+
 		let json_data = {
-			inReplyTo: messageId,
-			references: messageId,
-			from: from,
-			to: toRecipients,
+			inReplyTo: envelope.messageId,
+			references: envelope.messageId,
+			from: senderAddress,
+			to: toAddress,
+			subject: envelope.subject,
 			text: text,
 		};
 
@@ -105,54 +123,64 @@ export const createReplyOperation: IResourceOperationDef = {
 			message: Buffer;
 		};
 
-		const promise_compose_reply: Promise<ComposeMailResult> = new Promise((resolve, reject) => {
-			transporter.sendMail(json_data, (err, info) => {
-				if (err) {
-					resolve({
-						error: err,
-						info: null,
-					});
-					return;
-				} else {
-					if (!info.envelope || !info.messageId || !info.message) {
+		function compose_reply(
+			transporter: nodemailer.Transporter,
+			mailOptions: nodemailer.SendMailOptions,
+		): Promise<ComposeMailResult> {
+			return new Promise((resolve) => {
+				transporter.sendMail(mailOptions, (err, info) => {
+					if (err) {
+						resolve({ error: err, info: null });
+						return;
+					}
+
+					const messageBuffer = (info as any).message;
+					if (!info.envelope || !info.messageId || !messageBuffer) {
 						resolve({
-							error: new Error('Invalid info object returned by Nodemailer. Expected fields: envelope, messageId, message. Found: ' + JSON.stringify(info)),
+							error: new Error('Invalid info returned by Nodemailer.'),
 							info: null,
 						});
 						return;
 					}
 
-					let result: ComposeMailResult = {
+					resolve({
 						error: null,
-						info: info as ComposedEmailInfo,
-					};
-					resolve(result);
-				}
+						info: {
+							envelope: info.envelope,
+							messageId: info.messageId,
+							message: messageBuffer,
+						},
+					});
+				});
 			});
-		});
+		}
 
-		const result: ComposeMailResult = await promise_compose_reply;
+		const result = await compose_reply(transporter, json_data);
 
-		if (result.error) {
-			throw new NodeApiError(context.getNode(), {}, {
-				message: `Error composing the email: ${result.error}`,
-			});
+		if (result.error || !result.info) {
+			throw new NodeApiError(
+				context.getNode(),
+				{},
+				{
+					message: `Error composing the email: ${result.error?.message}`,
+				},
+			);
 		}
 
 		let rfc822Content = result.info!.message.toString('utf8');
 
-		await client.mailboxOpen(destinationMailboxPath, {readOnly: false});
-
-		const resp: AppendResonseObject = await client.append(
-			destinationMailboxPath,
-			rfc822Content,
-			['\\Reply']
-		)
+		const resp: AppendResonseObject = await client.append(destinationMailboxPath, rfc822Content, [
+			'\\Draft',
+		]);
 
 		if (!resp) {
-			throw new NodeApiError(context.getNode(), {}, {
-				message: "Unable to create reply. Unknown error",
-			});
+			throw new NodeApiError(
+				context.getNode(),
+				{},
+				{
+					message: 'Unable to create reply. Unknown error',
+				},
+			);
 		}
 
 		let item_json = JSON.parse(JSON.stringify(resp));
@@ -163,4 +191,4 @@ export const createReplyOperation: IResourceOperationDef = {
 
 		return returnData;
 	},
-}
+};
