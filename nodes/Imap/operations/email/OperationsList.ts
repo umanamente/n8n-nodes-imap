7f3802c3a4537482991baa90be41811d6feeb84d
Rinze de Laat <github@elit.nl>
2025-10-24 21:19:00 +0200

    Vibe coded IMAP delete added

--- nodes/Imap/operations/email/OperationsList.ts
+++ nodes/Imap/operations/email/OperationsList.ts
@@ -4,6 +4,7 @@ import { copyEmailOperation } from './functions/EmailCopy';
 import { createDraftOperation } from './functions/EmailCreateDraft';
 import { downloadOperation } from './functions/EmailDownload';
 import { downloadAttachmentOperation } from './functions/EmailDownloadAttachment';
+import { deleteEmailOperation } from './functions/EmailDelete';
 import { getEmailsListOperation } from './functions/EmailGetList';
 import { moveEmailOperation } from './functions/EmailMove';
 import { setEmailFlagsOperation } from './functions/EmailSetFlags';
@@ -14,6 +15,7 @@ export const emailResourceDefinitions: IResourceDef = {
 		getEmailsListOperation,
 		downloadOperation,
 		downloadAttachmentOperation,
+		deleteEmailOperation,
 		moveEmailOperation,
 		copyEmailOperation,
 		setEmailFlagsOperation,
